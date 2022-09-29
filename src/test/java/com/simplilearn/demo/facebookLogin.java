a45470499f951d773eb9105093623c7e9bae09b9
akanksha-cs <akankshapatwa24@gmail.com>
2022-09-29 23:18:41 +0530

    aaded

---
+++ src/test/java/com/simplilearn/demo/facebookLogin.java
@@ -0,0 +1,37 @@
+package com.simplilearn.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class facebookLogin {
+	
+	WebDriver driver;
+	@Test
+	public void facebook() {
+		
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("8573872658");
+		driver.findElement(By.id("pass")).sendKeys("Akki2658@");
+		driver.findElement(By.name("login")).submit();
+		
+		
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		driver= new ChromeDriver();
+	}
+	@AfterMethod
+	public void afterMethod() {
+		//driver.close();
+		driver=null;
+	}
+	
+	
+}
